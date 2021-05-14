53dd883c3f37262163990a63b04dec179e5aa7bf
learnwithsumit <play@learnwithsumit.com>
2021-05-14 10:42:00 +0600

    source code push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Counter from './components/Counter';
+import { useState } from 'react';
+import MyComponent from './components/MyComponent';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <Counter />
-            </div>
-        );
-    }
+export default function App() {
+    const [show, setShow] = useState(true);
+
+    return (
+        <div className="app">
+            <div>{show && <MyComponent />}</div>
+            <p>
+                <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+                    {show ? 'Hide post' : 'Show post'}
+                </button>
+            </p>
+        </div>
+    );
 }
