e11d78ae34a71364d1db840e6bffe8b8cdc51dac
mrhrifat <mrhrifat383759@gmail.com>
2021-11-25 12:19:40 +0600

    Remove parent path & use only relative path

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,13 +6,13 @@ export default function Posts() {
       <h1>Posts</h1>
       <ul>
         <li>
-          <Link to="/posts/1">Post 1</Link>
+          <Link to="1">Post 1</Link>
         </li>
         <li>
-          <Link to="/posts/2">Post 2</Link>
+          <Link to="2">Post 2</Link>
         </li>
         <li>
-          <Link to="/posts/3">Post 3</Link>
+          <Link to="3">Post 3</Link>
         </li>
       </ul>
     </div>
